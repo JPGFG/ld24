1334d454df3fa2e2c53ce9957fe20d6a51aa3b78
ckane144 <ckane144@gmail.com>
2024-10-05 13:20:51 -0400

    moved card stuff

---
+++ scripts/cards/pop_card.gd
@@ -0,0 +1,34 @@
+extends Card
+
+@onready var adder: Label = $TextureRect/Adder
+@onready var card_name: Label = $TextureRect/Card_Name
+@onready var description: Label = $TextureRect/Description
+@onready var rarity: Label = $TextureRect/Rarity
+@onready var multiplier: Label = $TextureRect/Multiplier
+@onready var germ_image: TextureRect = $TextureRect/Germ_Image
+@onready var card_image: TextureRect = $TextureRect/Card_Image
+
+func _ready() -> void:
+	build_card()
+	super()
+
+func build_card():
+	#if card_resource.multipler != null:
+	multiplier.text = str(card_resource.multipler) + "X"
+	
+	#if card_resource.adder != null:
+	adder.text = str(card_resource.adder) + "+"
+		
+	card_name.text = card_resource.name
+	description.text = card_resource.description
+	card_image.texture = card_resource.card_image
+	germ_image.texture = card_resource.germ_image
+	
+	# based off of card rarity update the backing and letter
+	match card_resource.rarity:
+		card_resource.Rarity.COMMON:
+			rarity.text = "C"
+		card_resource.Rarity.RARE:
+			rarity.text = "R"
+		card_resource.Rarity.LEGENDARY:
+			rarity.text = "L"
