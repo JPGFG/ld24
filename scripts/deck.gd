d170659ba68b9415244d8edd8ce35788d5145eef
ckane144 <ckane144@gmail.com>
2024-10-06 12:41:16 -0400

    added counter

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -3,10 +3,13 @@ class_name Deck
 
 var cards: Array[CardResource] = []
 @export var deck_resource: DeckResource
+@onready var deck_count: Label = $DeckCount
 
 func _ready():
 	for card in deck_resource.cards:
 		cards.append(card)
+	
+	deck_count.text = str(cards.size())
 
 func shuffle():
 	randomize()
@@ -15,6 +18,7 @@ func shuffle():
 func draw_card():
 	if cards.size() > 0:
 		var card = cards.pop_front()
+		deck_count.text = str(cards.size())
 		return card
 	else:
 		return null
